1d1615d7bb174ec7f7eaa432cf470d1390b9b58b
huangyu <1395928616@qq.com>
2024-03-28 14:44:02 +0800

    添加web-service

---
+++ functions/knative/web-service-go/hello.go
@@ -0,0 +1,15 @@
+package web_service_go
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
